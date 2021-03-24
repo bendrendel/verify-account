43c0fdb6cc106dd05bb27418109a8ce1f2919741
bendrendel <bfdrendel@gmail.com>
2021-03-23 22:43:43 -0600

    Prevent invalid values from being entered

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const inputs = document.querySelectorAll('input');
+const validValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Backspace'];
 
 inputs[0].focus();
 
@@ -16,7 +17,11 @@ inputs.forEach((input, idx) => {
     });
 
     input.addEventListener('keydown', e => {
-        if (e.target.value === '' && e.key === 'Backspace' && idx > 0) {
+        if (!validValues.includes(e.key)) {
+            e.preventDefault();
+        }
+
+        if (e.key === 'Backspace' && e.target.value === '' && idx > 0) {
             e.preventDefault();
             inputs[idx - 1].focus();
         }
