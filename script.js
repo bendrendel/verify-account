b6da637ba2e9ffc548a976bea1b969c0ff62fb48
bendrendel <bfdrendel@gmail.com>
2021-03-24 21:49:48 -0600

    Refactor event code

--- script.js
+++ script.js
@@ -4,26 +4,19 @@ const validValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Backspac
 inputs[0].focus();
 
 inputs.forEach((input, idx) => {
-    input.addEventListener('beforeinput', e => {
-        if (e.target.value !== '') {
+    input.addEventListener('keydown', e => {
+        if (!validValues.includes(e.key)) {
+            e.preventDefault();
+        } else if (e.target.value !== '') {
             e.target.value = '';
+        } else if (e.key === 'Backspace' && idx > 0) {
+            inputs[idx - 1].focus();
         }
     });
 
     input.addEventListener('input', e => {
         if (e.target.value !== '' && idx < inputs.length - 1) {
             inputs[idx + 1].focus();
-        }
-    });
-
-    input.addEventListener('keydown', e => {
-        if (!validValues.includes(e.key)) {
-            e.preventDefault();
-        }
-
-        if (e.key === 'Backspace' && e.target.value === '' && idx > 0) {
-            e.preventDefault();
-            inputs[idx - 1].focus();
         }
     });
 })
